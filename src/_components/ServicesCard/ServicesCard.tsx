81450a853374e5b1e892aaca28b71097ec5a9208
Maru Di Primio <mariana.diprimio@gmail.com>
2023-08-09 16:58:20 +0200

    add styles in services

--- src/_components/ServicesCard/ServicesCard.tsx
+++ src/_components/ServicesCard/ServicesCard.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import Styles from "./servicesCard.module.scss";
 
 type Props = {
     ImgSrc: string;
@@ -15,9 +16,9 @@ function ServicesCard({ ImgSrc, title, content, reverse }: Props) {
     return (
         <div style={{ display: "flex", flexDirection: `${reverse ? "row-reverse" : "row"}` }}>
             <Image src={ImgSrc} width={560} height={560} alt={title} layout="responsive"/>
-            <div>
-                <h2>{title}</h2>
-                <p>{content}</p>
+            <div className={Styles.cardContainer}>
+                <h2 className={Styles.cardContainer__title}>{title}</h2>
+                <p className={Styles.cardContainer__title}>{content}</p>
             </div>
         </div>
     );
