750fe303086a17488479108c9420a6568c35e696
Maru Di Primio <mariana.diprimio@gmail.com>
2023-08-09 16:58:55 +0200

    ourWorks styles in progress

--- src/_sections/OurWorks/OurWorks.tsx
+++ src/_sections/OurWorks/OurWorks.tsx
@@ -1,12 +1,13 @@
 import WorkCard from "@src/_components/WorkCard/WorkCard";
 import WorkToggleBtn from "@src/_components/WorkToggleBtn/WorkToggleBtn";
 import React from "react";
+import Styles from "./ourWorks.module.scss"
 
 function OurWorks() {
     const works = [{ text: "CARD 1 - WORK" }, { text: "CARD 2 - WORK" }, { text: "CARD 3 - WORK" }];
     return (
-        <section id="work">
-            <h2>OUR WORKS</h2>
+        <section className={Styles.ourWorksSection} id="work">
+            <h2  className={Styles.ourWorksSection__title}>OUR WORKS</h2>
             <WorkToggleBtn className={""} />
             {works.map((w, i) => (
                 <WorkCard key={i} content={w.text} />
