4359c785b6918e74f7d505dff8171d62ff527e8d
Agustín Carignano <agustin.carignano@gmail.com>
2023-08-11 21:22:10 +0200

    effect when mourse over work card

--- src/_sections/OurWorks/OurWorks.tsx
+++ src/_sections/OurWorks/OurWorks.tsx
@@ -7,22 +7,22 @@ import imgWork2 from "../../../public/images/work1.png";
 import imgWork3 from "../../../public/images/work1.png";
 
 function OurWorks() {
-  const works = [
-    { img: imgWork1, title: "Titulo del trabajo", text: " - Lo que hicimos" },
-    { img: imgWork2, title: "Titulo del trabajo", text: " - Lo que hicimos" },
-    { img: imgWork3, title: "Titulo del trabajo", text: " - Lo que hicimos" },
-  ];
-  return (
-    <section className={Styles.ourWorksSection} id="work">
-      <h2 className={Styles.ourWorksSection__title}>OUR WORKS</h2>
-      <WorkToggleBtn className={""} />
-      <div className={Styles.ourWorksSection__cardsContainer}>
-        {works.map((w, i) => (
-          <WorkCard key={i} title={w.title} text={w.text}  img={w.img} />
-        ))}
-      </div>
-    </section>
-  );
+    const works = [
+        { img: imgWork1, title: "Titulo del trabajo", text: " - Lo que hicimos" },
+        { img: imgWork2, title: "Titulo del trabajo", text: " - Lo que hicimos" },
+        { img: imgWork3, title: "Titulo del trabajo", text: " - Lo que hicimos" },
+    ];
+    return (
+        <section className={Styles.ourWorksSection} id="work">
+            <h2 className={Styles.ourWorksSection__title}>OUR WORKS</h2>
+            <WorkToggleBtn className={Styles.ourWorksSection__buttons} />
+            <div className={Styles.ourWorksSection__cardsContainer}>
+                {works.map((w, i) => (
+                    <WorkCard key={i} title={w.title} text={w.text} img={w.img} />
+                ))}
+            </div>
+        </section>
+    );
 }
 
 export default OurWorks;
