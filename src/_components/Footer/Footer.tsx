48e077acc1e1cfe152cac4fee0b384c99e13f2ba
Maru Di Primio <mariana.diprimio@gmail.com>
2023-08-11 13:25:56 +0200

    footer section ok

--- src/_components/Footer/Footer.tsx
+++ src/_components/Footer/Footer.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import Styles from "./footer.module.scss";
 import heart from "@public/images/heartIcon.svg";
 import Image from "next/image";
+import Utils from "../../styles/utils.module.scss"
 
 function Footer() {
     return (
-        <footer>
-            <h2>Gracias por visitar nuestra página, esperamos volver a verte pronto!</h2>
-            <p>
+        <footer className={Styles.footer}>
+            <h2 className={Styles.footer__title}><span className={Utils.highlightedText__fucsia}>Gracias</span> por visitar nuestra página, <br />esperamos volver a <span className={Utils.highlightedText__aqua}>verte pronto!</span></h2>
+            <p className={Styles.footer__text}>
                 Made with{" "}
                 <span>
-                    <Image src={heart} width={13} height={11} alt="" />
+                    <Image src={heart} width={15} height={13} alt="" />
                 </span>{" "}
                 <br />
                 Todos los derechos reservados
