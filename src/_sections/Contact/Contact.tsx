f5b1188320524d7c01a0b179c83acedcc5dee2f6
Maru Di Primio <mariana.diprimio@gmail.com>
2023-08-11 13:24:47 +0200

    contact section ok

--- src/_sections/Contact/Contact.tsx
+++ src/_sections/Contact/Contact.tsx
@@ -2,28 +2,44 @@ import Image from "next/image";
 import React from "react";
 import wsp from "@public/images/wspIcon.svg";
 import env from "@public/images/envIcon.svg";
+import contactImg from "@public/images/contactImage.png";
 import Link from "next/link";
+import Styles from "./contact.module.scss";
+import classNames from "classnames";
 
 function Contact() {
-    return (
-        <section id="contact">
-            <h2>CONTACT</h2>
-            <div>
-                <div>
-                    <Link href={"#"}>
-                        <Image src={wsp} width={50} height={50} alt="whatsapp icon" />
-                    </Link>
-                    <p>123 456 789</p>
-                </div>
-                <div>
-                    <Link href={"#"}>
-                        <Image src={env} width={50} height={50} alt="email icon" />
-                    </Link>
-                    <p>hello@mail.com</p>
-                </div>
-            </div>
-        </section>
-    );
+  // no funcionó lo de las combined clasess :( - llega undefined undefined
+  const combinedClassesLeft = `${Styles.contactSection__content} ${Styles["contactSection__content--left"]}`;
+  const combinedClassesRight = `${Styles.contactSection__content} ${Styles["contactSection__content--right"]}`;
+  return (
+    <section className={Styles.contactSection} id="contact">
+      <h2 className={Styles.contactSection__title}>CONTACT</h2>
+      <div className={Styles.contactSection__content}>
+        <div className={Styles.contactSection__content__left}>
+          <Image
+            src={contactImg}
+            width={443}
+            height={446}
+            alt="contact image"
+          />
+        </div>
+        <div className={Styles.contactSection__content__right}>
+          <div className={Styles.contactSection__content__right__item}>
+            <Link href={"#"}>
+              <Image src={wsp} width={40} height={40} alt="whatsapp icon" />
+            </Link>
+            <p>123 456 789</p>
+          </div>
+          <div className={Styles.contactSection__content__right__item}>
+            <Link href={"#"}>
+              <Image src={env} width={40} height={40} alt="email icon" />
+            </Link>
+            <p>hello@mail.com</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
