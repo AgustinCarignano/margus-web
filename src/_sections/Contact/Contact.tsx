7eea6410d1a9296ea01d5db69889289622074db3
Agustín Carignano <agustin.carignano@gmail.com>
2023-08-12 13:05:40 +0200

    added wsp icon hover effect

--- src/_sections/Contact/Contact.tsx
+++ src/_sections/Contact/Contact.tsx
@@ -2,15 +2,16 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import wsp from "@public/images/wspIcon.svg";
+import wsp_hover from "@public/images/wspIcon_hover.svg";
 import env from "@public/images/envIcon.svg";
 import env_hover from "@public/images/envIcon_hover.svg";
 import contactImg from "@public/images/contactImage.png";
 import Link from "next/link";
 import Styles from "./contact.module.scss";
-import classNames from "classnames";
 
 function Contact() {
     const [envHover, setEnvHover] = useState(false);
+    const [wspHover, setWspHover] = useState(false);
 
     // no funcionó lo de las combined clasess :( - llega undefined undefined
     const combinedClassesLeft = `${Styles.contactSection__content} ${Styles["contactSection__content--left"]}`;
@@ -24,8 +25,8 @@ function Contact() {
                 </div>
                 <div className={Styles.contactSection__content__right}>
                     <div className={Styles.contactSection__content__right__item}>
-                        <Link href={"#"}>
-                            <Image src={wsp} width={40} height={40} alt="whatsapp icon" />
+                        <Link href={"#"} onMouseOver={() => setWspHover(true)} onMouseLeave={() => setWspHover(false)}>
+                            {!wspHover ? <Image src={wsp} width={40} height={40} alt="whatsapp icon" /> : <Image src={wsp_hover} width={40} height={40} alt="whatsapp icon" />}
                         </Link>
                         <p>123 456 789</p>
                     </div>
